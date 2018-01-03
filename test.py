c950dac34b07a745a6931cac49842eba87c309bc
root <1073820320@qq.com>
2018-01-03 23:45:49 +0800

    update for test

---
+++ test.py
@@ -0,0 +1,56 @@
+from PIL import Image, ImageFont, ImageDraw
+import os
+import math
+
+pro_path = "/home/sun/code/WeChatOA"
+im_path = "/home/sun/code/WeChatOA/timg.jpeg"
+font = ImageFont.load_default()
+
+def add_text2img(img, text, font= font):
+    img_draw = ImageDraw.Draw(img)
+
+    for x in range(im.size[0]):
+        for y in range(im.size[1]):
+            if not in_center(im.size[0] / 2, im.size[1] / 2, min(im.size) / 2, x, y):
+                img_draw.point((x, y), fill=(255, 255, 255, 0))
+
+    # if img.size[0] < img.size[1]:
+    #     x0 = 0
+    #     x1 = img.size[0]
+    #     y0 = (img.size[1] - x1) / 2
+    #     y1 = (img.size[1] + x1) / 2
+    # else:
+    #     y0 = 0
+    #     y1 = img.size[1]
+    #     x0 = img.size[0] - y1 / 2
+    #     x1 = img.size[0] + y1 / 2
+    # img_draw.arc([x0, y0, x1, y1], 0, 360, fill= (0, 0, 0, 0))
+
+    text_size_x, text_size_y = img_draw.textsize(text, font)
+    print(text_size_x, text_size_y)
+    text_xy = ((img.size[0] - font.getsize(text)[0]) / 2, (img.size[1] - font.getsize(text)[1]) / 2)
+    print(text_xy)
+    img_draw.text(text_xy, text, font= font, fill="red")
+
+
+
+def in_center(center_x, center_y, r, x, y):
+    return math.pow(math.pow(math.fabs(center_x - x), 2) + math.pow(math.fabs(center_y - y), 2), 0.5) < r
+
+
+if __name__ == '__main__':
+    im = Image.open(im_path)
+    # im.show()
+    # im = im.rotate(45)#.show()
+    print(im.size)
+
+
+
+
+    font_path = os.path.join(pro_path, "/fonts/Ubuntu-C.ttf")
+    font = ImageFont.truetype(font_path, 80)
+    add_text2img(im, "sunhaihong", font)
+    # im.show()
+    # im.rotate(-45).show()
+    im.save("circle7.jpg")
+    # im.rotate(135).show()
